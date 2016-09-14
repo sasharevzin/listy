e23aff3ebdad44e8ad0d8fd0bfbcd007d0d42b9f
Sasha <sasha.nike@gmail.com>
2016-09-14 05:55:25 -0400

    Revert "Add tests"

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,4 @@
 class HomeController < ApplicationController
   def index
-    render text: __FILE__
   end
 end
--- spec/models/user_spec.rb
+++
@@ -1,9 +0,0 @@
-require "rails_helper"
-
-RSpec.describe User, type: :model do
-  let(:user){ FactoryGirl.create(:user) }
-
-  it "should have full_name" do
-    expect(user.full_name).to eq("#{user.first_name} #{user.last_name}")
-  end
-end
